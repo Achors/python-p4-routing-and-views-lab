9a3894fc6e3a5bb6a9190f9a18d744ced68c9841
Adolf <adolfnyerere@gmail.com>
2024-01-09 02:11:18 +0300

    passed 5

--- server/app.py
+++ server/app.py
@@ -4,13 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
-
 
 @app.route('/print/<string:param>')
 def print_string(param):
@@ -19,5 +15,34 @@ def print_string(param):
 
 @app.route('/count/<int:param>')
 def count(param):
-    num = '\n'.join(map(str, range(1, param + 1)))
-    return f'<pre>{num}</pre>'
+    numbers = '\n'.join(map(str, range(1, param + 1)))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<float:num1>/<string:operation>/<float:num2>')
+def math(num1, operation, num2):
+    result = perform_operation(num1, operation, num2)
+    return f'<!doctype html><title>Math Result</title><h1>{result}</h1>'
+
+def perform_operation(num1, operation, num2):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Error: Division by zero'
+        else:
+            return num1 / num2
+    elif operation == '%':
+        if num2 == 0:
+            return 'Error: Modulo by zero'
+        else:
+            return num1 % num2
+    else:
+        return 'Error: Invalid operation'
+    
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
