3cbf99546a7a0c9c8c4270f1d6006d570f04db29
Adolf <adolfnyerere@gmail.com>
2024-01-09 01:45:49 +0300

    progress

--- server/app.py
+++ server/app.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return f'<h1>{param}</h1>'
+
+@app.route('/count/<int:param>')
+def count(param):
+    num = '\n'.join(map(str, range(1, param + 1)))
+    return f'<pre>{num}</pre>'
